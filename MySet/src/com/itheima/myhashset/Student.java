6daed9c306720fdeaf893d60fb606f97d16c07eb
Kris <abraham.bowen@gmail.com>
2022-11-02 17:34:30 +0800

    mylistdemo1

---
+++ MySet/src/com/itheima/myhashset/Student.java
@@ -0,0 +1,50 @@
+package com.itheima.myhashset;
+
+import java.util.Objects;
+
+public class Student {
+    private String name;
+    private int age;
+
+    public Student() {
+    }
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    // 我们可以对Object类中的hashCode方法进行重写。
+    // 在重写之后，就一般是根据对象的属性值来计算哈希值的。
+    // 此时跟对象的地址值就没有任何关系额。
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + age;
+        return result;
+    }
+}
