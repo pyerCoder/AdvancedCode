a4c818c20e4ca2cbc41311490d0d7acb5b771ab0
Kris <abraham.bowen@gmail.com>
2022-11-04 05:24:37 +0800

    mylistdemo1

--- filemodule/src/com/itheima/filedemo/Filedemo2.java
+++ filemodule/src/com/itheima/filedemo/Filedemo2.java
@@ -1,15 +1,19 @@
 package com.itheima.filedemo;
 
 import java.io.File;
+import java.io.IOException;
 
 public class Filedemo2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // 这个路径固定不变了
         File file = new File("/Users/kris/Work/Study/Java/Heima/AdvancedCode");
         System.out.println(file);
 
         // 当前项目下的a.txt
         File file2 = new File("a.txt");
+        // 测试文件创建与删除，观察对比该文件所在目录
+//        file2.createNewFile();
+//        file2.delete();
         System.out.println(file2);
 
         // 当前项目下 ---- 指定模块下的a.txt
